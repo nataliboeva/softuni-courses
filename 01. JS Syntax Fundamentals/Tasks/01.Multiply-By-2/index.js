c0a7985ee87d2b0d692fe44881c147bcbf30afae
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2025-02-28 18:50:44 +0200

    Delete 01. JS Syntax Fundamentals directory

--- 01. JS Syntax Fundamentals/Tasks/01.Multiply-By-2/index.js
+++
@@ -1,7 +0,0 @@
-function solve(num){
-    console.log(num * 2);
-}
-
-solve(2);
-solve(5);
-solve(20);
--- 01. JS Syntax Fundamentals/Tasks/10.Numbers-from-M-to-N/index.js
+++
@@ -1,7 +0,0 @@
-function print(num1, num2){
-    for(let i = num1; i>=num2; i--){
-        console.log(i);
-    }
-}
-print(6, 2);
-print(4,1);
