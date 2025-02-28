301c76eedbba1077ceaf5f1bad3d517552235f03
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2025-02-28 18:50:59 +0200

    Delete 03. JS Arrays & Strings directory

--- 03. JS Arrays & Strings/Tasks/04. Substring/index.js
+++
@@ -1,7 +0,0 @@
-function substring(word, startIndex, count){
-    const endIndex = startIndex + count;
-    const result = word.substring(startIndex, endIndex);
-    console.log(result);
-}
-substring('ASentence', 1, 8);
-substring('SkipWord', 4, 7);
