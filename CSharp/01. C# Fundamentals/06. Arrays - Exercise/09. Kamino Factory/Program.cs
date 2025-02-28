9eba6d641608a91c15201503387bbf8c8c4ad6c1
Natali Boeva <natali9.boeva@abv.bg>
2025-02-28 17:38:53 +0200

    This folder contains my work related to fundamental c# programming.

---
+++ CSharp/01. C# Fundamentals/06. Arrays - Exercise/09. Kamino Factory/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _09._Kamino_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine().Split('!');  
+        }
+    }
+}
