10d2eb8f893fcf18977287860fd0fd11f1432e0f
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2025-02-28 18:51:05 +0200

    Delete 04. JS Arrays & Strings - Excercise directory

--- 04. JS Arrays & Strings - Excercise/Tasks/03. List Of Names/index.js
+++
@@ -1,8 +0,0 @@
-function solve(names){
-    names.sort((a,b) => a.localeCompare(b))
-    .map((name, index) => {
-        console.log(`${index + 1}.${name}`)
-    });
-}
-
-solve(['John', 'Bob', 'Christina', 'Ema']);
--- 04. JS Arrays & Strings - Excercise/Tasks/06. Modern Times of #(HashTag)/index.js
+++
@@ -1,7 +0,0 @@
-function solve(text){
-    const pattern = /#[A-Za-z]+/gm;
-    const words = text.match(pattern);
-    for (let word of words) console.log(word.substring(1));
-}
-solve('Nowadays everyone uses # to tag a #special word in #socialMedia');
-solve('The symbol # is known #variously in English-speaking #regions as the #number sign');
