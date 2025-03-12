66548a6360ebb0589361f65c782d7fb92133d9fa
Natali Boeva <natali9.boeva@abv.bg>
2025-03-12 22:31:33 +0200

    Added problem 04 and problem 05

--- Entity-Framework-Core/01. Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
+++ Entity-Framework-Core/01. Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -39,6 +39,57 @@ namespace SoftUni
             return sb.ToString().TrimEnd();
 
         }
+
+        //Problem 04
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context
+                .Employees
+                .OrderBy(e => e.FirstName)
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary.ToString("F2")}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 05
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context
+                .Employees
+                .Where(e => e.Department.Name.Equals("Research and Development"))
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepatmentName = e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from Research and Development - ${e.Salary.ToString("F2")}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
  
