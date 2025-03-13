355ac4bd56c7d0d2afcfe8f76bd462f7ec8faf2d
Natali Boeva <natali9.boeva@abv.bg>
2025-03-13 11:29:31 +0200

    Added Problem 06 and Problem 07

--- Entity-Framework-Core/01. Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
+++ Entity-Framework-Core/01. Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using SoftUni.Data;
+using SoftUni.Models;
 using System.Text;
 
 namespace SoftUni
@@ -88,6 +89,79 @@ namespace SoftUni
                 sb.AppendLine($"{e.FirstName} {e.LastName} from Research and Development - ${e.Salary.ToString("F2")}");
             }
 
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 06
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            const string newAddressText = "Vitoshka 15";
+            const int newAddressTownId = 4;
+
+            Address newAddress = new Address()
+            {
+                AddressText = newAddressText,
+                TownId = newAddressTownId
+            };
+
+            Employee nakovEmployee = context
+                .Employees
+                .First(e => e.LastName.Equals("Nakov"));
+            nakovEmployee.Address = newAddress;
+
+            context.SaveChanges();
+
+            string?[] addresses = context
+                .Employees
+                .Where(e => e.AddressId.HasValue)
+                .OrderByDescending(e => e.AddressId)
+                .Select(e => e.Address!.AddressText)
+                .Take(10)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, addresses);            
+        }
+        //Problem 07
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employeesWithProject = context
+                .Employees
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    ManagerFirstName = e.Manager == null ? null : e.Manager.FirstName,
+                    ManagerLastName = e.Manager == null ? null : e.Manager.LastName,
+                    Projects = e.EmployeesProjects
+                    .Select(ep => ep.Project)
+                    .Where(p => p.StartDate.Year >= 2001 && p.StartDate.Year <= 2003)
+                    .Select(p => new
+                    {
+                        ProjectName = p.Name,
+                        p.StartDate,
+                        p.EndDate
+                    })
+                    .ToArray()
+                })
+                .Take(10)
+                .ToArray();
+
+            foreach (var e in employeesWithProject)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+                foreach (var p in e.Projects)
+                {
+                    string startDateFormatted = p.StartDate
+                       .ToString("M/d/yyyy h:mm:ss tt");
+                    string endDateFormatted = p.EndDate.HasValue ?
+                        p.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt") : "not finished";
+                    sb
+                        .AppendLine($"--{p.ProjectName} - {startDateFormatted} - {endDateFormatted}");
+                }
+            }
             return sb.ToString().TrimEnd();
         }
     }
