31efc1671743fe81d547dd5894c2da5cbf48fe4e
Natali Boeva <natali9.boeva@abv.bg>
2025-03-15 23:15:53 +0200

    Entity Relations

---
+++ Entity-Framework-Core/02. Entity Relations/P02_FootballBetting/P02_FootballBetting.Common/EntityValidationConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace P02_FootballBetting.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Team
+        {
+            public const int TeamNameMaxLength = 75;
+
+            public const int TeamLogoUrlMaxLength = 2048;
+
+
+        }
+    }
+}
---
+++ Entity-Framework-Core/02. Entity Relations/P02_FootballBetting/P02_FootballBetting.Data.Models/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Common.EntityValidationConstants.Team;
+    public class Team
+    {
+        
+        [Key]
+        public int TeamId { get; set; }
+
+        [Required]
+        [MaxLength(TeamNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(TeamLogoUrlMaxLength)]
+        public string LogoUrl { get; set; }
+
+        [Required]
+        [MaxLength(TeamNameMaxLength)]
+        public string Inititals { get; set; } = null!;
+        public string Budget { get; set; }
+        public string PrimaryKitColorId { get; set; }
+        public string SecondaryKitColorId { get; set;}
+    }
+}
