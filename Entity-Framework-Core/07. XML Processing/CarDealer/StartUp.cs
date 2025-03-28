45fc131da44cf7d79ee46d276b376949ce1d21b8
Natali Boeva <natali9.boeva@abv.bg>
2025-03-28 23:57:22 +0200

    Added Problem 09

--- Entity-Framework-Core/07. XML Processing/CarDealer/StartUp.cs
+++ Entity-Framework-Core/07. XML Processing/CarDealer/StartUp.cs
@@ -1,5 +1,11 @@
-﻿using CarDealer.Data;
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
 
 namespace CarDealer
 {
@@ -10,6 +16,62 @@ namespace CarDealer
             using CarDealerContext dbContext = new CarDealerContext();
             dbContext.Database.Migrate();
             Console.WriteLine("Database migrated to the latest version successfully");
+
+            const string xmlFilePath = "../../../Datasets/suppliers.xml";
+            string inputXml = File.ReadAllText(xmlFilePath);
+
+            string result = ImportSuppliers(dbContext, inputXml);
+            Console.WriteLine(result);
+        }
+        //Problem 09
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            string result = string.Empty;
+
+            ImportSupplierDto[]? supplierDtos = XmlHelper
+                .Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+            if (supplierDtos != null)
+            {
+                ICollection<Supplier> validSuppliers = new List<Supplier>();
+                foreach (ImportSupplierDto supplierDto in supplierDtos)
+                {
+                    if (!IsValid(supplierDto))
+                    {
+                        continue;
+                    }
+
+                    bool isImporterValid = bool
+                        .TryParse(supplierDto.IsImporter, out bool isImporter);
+                    if (!isImporterValid)
+                    {
+                        continue;
+                    }
+
+                    Supplier supplier = new Supplier()
+                    {
+                        Name = supplierDto.Name,
+                        IsImporter = isImporter
+                    };
+                    validSuppliers.Add(supplier);
+                }
+
+                context.Suppliers.AddRange(validSuppliers);
+                context.SaveChanges();
+
+                result = $"Successfully imported {validSuppliers.Count}";
+            }
+
+            return result;
+        }
+        private static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator
+                .TryValidateObject(dto, validateContext, validationResults, true);
+
+            return isValid;
         }
     }
 }
