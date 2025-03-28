ecdfbf17cb951e03821a5e85b6b68f8fa0cddc69
Natali Boeva <natali9.boeva@abv.bg>
2025-03-28 22:44:16 +0200

    Upload code first for initial DB creation

---
+++ Entity-Framework-Core/07. XML Processing/CarDealer/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public DateTime BirthDate { get; set; }
+
+        public bool IsYoungDriver { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; } 
+            = new HashSet<Sale>(); 
+    }
+}
---
+++ Entity-Framework-Core/07. XML Processing/CarDealer/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using CarDealer.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using CarDealerContext dbContext = new CarDealerContext();
+            dbContext.Database.Migrate();
+            Console.WriteLine("Database migrated to the latest version successfully");
+        }
+    }
+}
